d8493b25e8a0ed1c4971f20416ddf369c18a1928
jonnyhoudini <jmcaveety@gmail.com>
2022-06-23 15:46:14 +0100

    changed title again

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleBar = ({ handleSelectChange, genres }) => {
   return (
     <div>
-      <h1>Official Singles Chart</h1>
+      <h1>Singles Chart</h1>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
